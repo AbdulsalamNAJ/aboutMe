ac2de86ed542165e1dd2bbe41218726606c1465d
Anas Alamro <alamroanas@gmail.com>
2021-05-24 16:06:12 +0300

    asking user

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
 'use strict';
-alert ('answer yes or no')
+
 
 
 let mycar = prompt('do you know which my favorite car?');
-mycar.toLowerCase();
-
 console.log (mycar);
 
+mycar.toLowerCase();
+
+if (mycar === 'yes') {
+ alert('answer correct');   
+}else if (mycar === 'no') {
+    console.log('correct answer');
+}else{
+    alert('answer yes or no')
+}
 let myage = prompt('do you know what is my age?');
 console.log(myage);
+if (myage === 'yes') {
+    alert('answer correct');   
+   }else if (myage === 'no') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
 let mybook = prompt('do you know my book?');
 console.log(mybook);
+if (mybook === 'yes') {
+    alert('answer correct');   
+   }else if (mybook === 'no') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
 let mycontry = prompt('do you know my contry?');
 console.log(mycontry);
+if (mycontry === 'yes') {
+    alert('answer correct');   
+   }else if (mycontry === 'no') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
 let myfather = prompt('do you know what is my father name?');
 console.log(myfather);
+if (myfather === 'yes') {
+    alert('answer correct');   
+   }else if (myfather === 'no') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
