148e80cf673cc59f0954af5386b0560f7d5c4d98
Anas Alamro <alamroanas@gmail.com>
2021-05-24 15:23:18 +0300

    about me

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+alert ('answer yes or no')
+
+
+let mycar = prompt('do you know which my favorite car?');
+mycar.toLowerCase();
+
+console.log (mycar);
+
+let myage = prompt('do you know what is my age?');
+console.log(myage);
+
+let mybook = prompt('do you know my book?');
+console.log(mybook);
+
+let mycontry = prompt('do you know my contry?');
+console.log(mycontry);
+
+let myfather = prompt('do you know what is my father name?');
+console.log(myfather);
