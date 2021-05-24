2c6a692ae6a11322f31f3ec0b9dbd8c1c8a55449
Anas Alamro <alamroanas@gmail.com>
2021-05-24 16:19:50 +0300

    save change

--- js/app.js
+++ js/app.js
@@ -7,18 +7,18 @@ console.log (mycar);
 
 mycar.toLowerCase();
 
-if (mycar === 'yes') {
+if (mycar === 'yes' || mycar === 'y')  {
  alert('answer correct');   
-}else if (mycar === 'no') {
+}else if (mycar === 'no' || mycar === 'n') {
     console.log('correct answer');
 }else{
     alert('answer yes or no')
 }
 let myage = prompt('do you know what is my age?');
 console.log(myage);
-if (myage === 'yes') {
+if (myage === 'yes' || myage === 'y') {
     alert('answer correct');   
-   }else if (myage === 'no') {
+   }else if (myage === 'no' || myage === 'n') {
        console.log('correct answer');
    }else{
     alert('answer yes or no')
@@ -26,9 +26,9 @@ if (myage === 'yes') {
 
 let mybook = prompt('do you know my book?');
 console.log(mybook);
-if (mybook === 'yes') {
+if (mybook === 'yes' || mybook === 'y') {
     alert('answer correct');   
-   }else if (mybook === 'no') {
+   }else if (mybook === 'no' || mybook === 'n') {
        console.log('correct answer');
    }else{
     alert('answer yes or no')
@@ -36,9 +36,9 @@ if (mybook === 'yes') {
 
 let mycontry = prompt('do you know my contry?');
 console.log(mycontry);
-if (mycontry === 'yes') {
+if (mycontry === 'yes' || mycontry === 'y') {
     alert('answer correct');   
-   }else if (mycontry === 'no') {
+   }else if (mycontry === 'no' || mycontry === 'n') {
        console.log('correct answer');
    }else{
     alert('answer yes or no')
@@ -46,9 +46,9 @@ if (mycontry === 'yes') {
 
 let myfather = prompt('do you know what is my father name?');
 console.log(myfather);
-if (myfather === 'yes') {
+if (myfather === 'yes' || myfather === 'y') {
     alert('answer correct');   
-   }else if (myfather === 'no') {
+   }else if (myfather === 'no' || myfather === 'n') {
        console.log('correct answer');
    }else{
     alert('answer yes or no')
