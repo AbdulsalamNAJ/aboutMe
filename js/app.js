b6a06081baa44f982a031540905b590234d5dfdc
Anas Alamro <alamroanas@gmail.com>
2021-05-25 22:00:57 +0300

    new quastion

--- js/app.js
+++ js/app.js
@@ -2,57 +2,57 @@
 
 
 
-// let mycar = prompt('do you know which my favorite car?');
-// console.log (mycar);
+let mycar = prompt('do you know which my favorite car?');
+console.log (mycar);
 
-// mycar.toLowerCase();
+mycar.toLowerCase();
 
-// if (mycar === 'yes' || mycar === 'y')  {
-//  alert('answer correct');   
-// }else if (mycar === 'no' || mycar === 'n') {
-//     console.log('correct answer');
-// }else{
-//     alert('answer yes or no')
-// }
-// let myage = prompt('do you know what is my age?');
-// console.log(myage);
-// if (myage === 'yes' || myage === 'y') {
-//     alert('answer correct');   
-//    }else if (myage === 'no' || myage === 'n') {
-//        console.log('correct answer');
-//    }else{
-//     alert('answer yes or no')
-//    }
+if (mycar === 'yes' || mycar === 'y')  {
+ alert('answer correct');   
+}else if (mycar === 'no' || mycar === 'n') {
+    console.log('correct answer');
+}else{
+    alert('answer yes or no')
+}
+let myage = prompt('do you know what is my age?');
+console.log(myage);
+if (myage === 'yes' || myage === 'y') {
+    alert('answer correct');   
+   }else if (myage === 'no' || myage === 'n') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
-// let mybook = prompt('do you know my book?');
-// console.log(mybook);
-// if (mybook === 'yes' || mybook === 'y') {
-//     alert('answer correct');   
-//    }else if (mybook === 'no' || mybook === 'n') {
-//        console.log('correct answer');
-//    }else{
-//     alert('answer yes or no')
-//    }
+let mybook = prompt('do you know my book?');
+console.log(mybook);
+if (mybook === 'yes' || mybook === 'y') {
+    alert('answer correct');   
+   }else if (mybook === 'no' || mybook === 'n') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
-// let mycontry = prompt('do you know my contry?');
-// console.log(mycontry);
-// if (mycontry === 'yes' || mycontry === 'y') {
-//     alert('answer correct');   
-//    }else if (mycontry === 'no' || mycontry === 'n') {
-//        console.log('correct answer');
-//    }else{
-//     alert('answer yes or no')
-//    }
+let mycontry = prompt('do you know my contry?');
+console.log(mycontry);
+if (mycontry === 'yes' || mycontry === 'y') {
+    alert('answer correct');   
+   }else if (mycontry === 'no' || mycontry === 'n') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
-// let myfather = prompt('do you know what is my father name?');
-// console.log(myfather);
-// if (myfather === 'yes' || myfather === 'y') {
-//     alert('answer correct');   
-//    }else if (myfather === 'no' || myfather === 'n') {
-//        console.log('correct answer');
-//    }else{
-//     alert('answer yes or no')
-//    }
+let myfather = prompt('do you know what is my father name?');
+console.log(myfather);
+if (myfather === 'yes' || myfather === 'y') {
+    alert('answer correct');   
+   }else if (myfather === 'no' || myfather === 'n') {
+       console.log('correct answer');
+   }else{
+    alert('answer yes or no')
+   }
 
 let mylevelcourse = prompt('what is my level course?');
 let answer6=mylevelcourse;
